bd62fa2161bb681e6e6bc0df2eeef271ab36a95f
odessabear@gmail.com <odessabear@gmail.com>
2018-07-18 18:40:09 +0300

    volatile and synchronized lessons

---
+++ src/ThreadTest1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class ThreadTest1 {
+    public static void main(String[] args) {
+        MyThread1 myThread1 = new MyThread1();
+        myThread1.start();
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        myThread1.shutdown();
+    }
+}
+
+class MyThread1 extends Thread {
+    private volatile boolean running = true;
+    @Override
+    public void run() {
+        while (running) {
+            System.out.println("Hello");
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void shutdown(){
+
+        this.running = false;
+    }
+}
