fe326e653db8a2d4c472f1e7faa093ba1f3bda97
odessabear@gmail.com <odessabear@gmail.com>
2018-07-25 18:45:06 +0300

    producer-consumer lesson part2

---
+++ src/WaitAndNotifyTest.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+public class WaitAndNotifyTest {
+    public static void main(String[] args) throws InterruptedException {
+        WaitAndNotify waitAndNotify = new WaitAndNotify();
+
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    waitAndNotify.produce();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    waitAndNotify.consume();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+    }
+}
+
+class WaitAndNotify {
+    public void produce() throws InterruptedException {
+        synchronized (this) {
+            System.out.println("Producer thread started...");
+            wait();
+            System.out.println("Producer thread resumed...");
+        }
+    }
+
+    public void consume() throws InterruptedException {
+        Thread.sleep(2000);
+        Scanner scanner = new Scanner(System.in);
+
+        synchronized (this){
+            System.out.println("Waiting for return key pressed");
+            scanner.nextLine();
+            notify();
+        }
+    }
+}
