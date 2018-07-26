147b73542d7d4a9d3d7d2c07152ebdbe902f331e
odessabear@gmail.com <odessabear@gmail.com>
2018-07-26 19:31:57 +0300

    last lessons about threads

---
+++ src/InterruptThreadsTest.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+
+public class InterruptThreadsTest {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Random random = new Random();
+                for (int i = 0; i < 1000000000; i++) {
+                    if (Thread.currentThread().isInterrupted()) {
+                        System.out.println("Thread was interrupted");
+                        break;
+                    }
+                    Math.sin(random.nextDouble());
+
+                }
+            }
+        });
+
+        System.out.println("Starting of thread");
+
+        thread.start();
+
+        thread.interrupt();
+        Thread.sleep(1000);
+
+        thread.join();
+
+
+        System.out.println("Thread finished");
+    }
+}
